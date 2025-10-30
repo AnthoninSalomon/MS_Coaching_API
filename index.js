ab4c28051144a714089edec701f87300e8233d41
Anthonin <anthonin.salomon@gmail.com>
2025-10-30 19:07:26 +0100

    Ajout fonction création d'utilisateur

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config(); 
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const { Pool } = pg;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
+app.use(express.json());
+
+// Route pour créer un utilisateur
+app.post("/create-user", async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO \"User\" (\"FirstName\", \"LastName\", \"Email\", \"Password\") VALUES ($1, $2, $3, $4) RETURNING *",
+      [firstName, lastName, email, password]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Erreur lors de la création de l’utilisateur" });
+  }
+});
+
+//Test
 app.get("/", function (req, res) {
   res.send("API MS Coaching");
 });
